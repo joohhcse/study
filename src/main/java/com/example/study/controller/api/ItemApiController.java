6e22c9102294bb2649cb5cb4b17181d3afc4f02c
joohhcse <joohhcse@gmail.com>
2022-10-05 21:05:07 +0900

    abstract Controller

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,42 +1,52 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.ItemApiRequest;
 import com.example.study.model.network.response.ItemApiResponse;
 import com.example.study.service.ItemApiLogicService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/item")
-@RequiredArgsConstructor
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+//@RequiredArgsConstructor
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
-    private final ItemApiLogicService itemApiLogicService;
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")    // /api/item
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}") // /api/item/1 ... 1000
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
 
-    @Override
-    @PutMapping("")// /api/item
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")// /api/item/1 ... 1000
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
-    }
+//    @Override
+//    @PostMapping("")    // /api/item
+//    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}") // /api/item/1 ... 1000
+//    public Header<ItemApiResponse> read(@PathVariable Long id) {
+//        return itemApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("")// /api/item
+//    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}")// /api/item/1 ... 1000
+//    public Header delete(@PathVariable Long id) {
+//        return itemApiLogicService.delete(id);
+//    }
 
 }
--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -1,43 +1,55 @@
 package com.example.study.controller.api;
 
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.response.OrderGroupApiResponse;
 import com.example.study.service.OrderGroupApiLogicService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/orderGroup")
 @RequiredArgsConstructor
-public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
 
-    private final OrderGroupApiLogicService orderGroupApiLogicService;
+    @Autowired
+    private OrderGroupApiLogicService orderGroupApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = orderGroupApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
-    }
+//    private final OrderGroupApiLogicService orderGroupApiLogicService;
+//
+//    @Override
+//    @PostMapping("")
+//    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
+//        return orderGroupApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}")
+//    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+//        return orderGroupApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("")
+//    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
+//        return orderGroupApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}")
+//    public Header delete(@PathVariable Long id) {
+//        return orderGroupApiLogicService.delete(id);
+//    }
 
 }
